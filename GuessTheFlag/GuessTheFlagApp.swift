684eb62c78e7a7ee0f4c2b6960bf682080d1197c
ino yang popper <ino.yang.p@gmail.com>
2025-01-28 15:04:07 -0800

    created GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ino Yang Popper on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
