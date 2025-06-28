c73f6a6c5fc665ef48a7e40da9a230a100198c59
ino yang popper <ino.yang.p@gmail.com>
2025-06-27 22:40:51 -0700

    added accessibility

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -34,6 +34,20 @@ struct ContentView: View {
     // New state variable to track the selected flag
     @State private var selectedFlag: Int? = nil
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -77,6 +91,7 @@ struct ContentView: View {
                                 .opacity(selectedFlag == nil || selectedFlag == number ? 1 : 0.25) // Fade out non-selected flags
                                 .animation(.easeInOut(duration: 1), value: selectedFlag)
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                 }
                 .frame(maxWidth: .infinity)
